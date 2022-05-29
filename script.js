847b51e63351aa6cf02384aa124ee334e3852d89
Iran Filho <Iran_filho@yahoo.com.br>
2022-05-29 19:49:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ window.onload = function(){
     ctx = canvas.getContext("2d")
 
     //variáveis
-
     snake = []
     positionX = 10
     positionY = 10
     foodX = 15
-    fooxY = 15
+    foodY = 15
     velX = 0
     velY = 0
     grid = 20
@@ -16,6 +15,32 @@ window.onload = function(){
 
     //Chamada da função jogo a cada 100ms
     setInterval(jogo,100)
+
+    //controles
+    document.addEventListener("keydown", function(e){
+        switch(e.keyCode){
+            //Seta direita
+            case 39:
+                velX = 1
+                velY = 0
+                break
+            //Seta esquerda
+            case 37:
+                velX = -1
+                velY = 0
+                break
+            //Seta cima
+            case 38:
+                velY = -1
+                velX = 0
+                break
+            //Seta baixo
+            case 40:
+                velY = 1
+                velX = 0
+                break            
+        }
+    })
 }
 
 function jogo(){
@@ -24,13 +49,52 @@ function jogo(){
     //Distância da borda horizontal, distância da borda vertical, largura, altura
     ctx.fillRect(0,0,canvas.width, canvas.height)
 
-    //Posicionando a cobra
-    snake.push({x: positionX, y: positionY})
-    console.log(snake[0])
+    //Deslocamento da cobra
+    positionX +=velX
+    positionY +=velY
+
+    //Espelhamento
+    if(positionX <0){
+        positionX = grid
+    }
+    if(positionX>grid){
+        positionX=0
+    }
+
+    if(positionY <0){
+        positionY = grid
+    }
+    if(positionY>grid){
+        positionY=0
+    }
 
     //Configuração da Cobra
     ctx.fillStyle = "#00f102"
     for(let i=0; i<snake.length; i++){
         ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1)
+        if(snake[i].x==positionX && snake[i].y==positionY){
+            tam=3
+        }
     }
+
+    //Posicionando a cobra
+    snake.push({x: positionX, y: positionY})
+    console.log(snake[0])
+
+    //Apagando
+    while(snake.length > tam){
+        snake.shift()
+    }
+
+    //Configurando a comida
+    ctx.fillStyle = "#F1C40F"
+    ctx.fillRect(foodX*grid, foodY*grid, grid-1, grid-1)
+
+    //comendo a comida
+    if(positionX==foodX && positionY==foodY){
+        tam++
+        foodX=Math.floor(Math.random()*grid)
+        foodY=Math.floor(Math.random()*grid)
+    }
+
 }
